07da5600f57567b4d0f1f111d851a1c1fddcc950
lee-sunah <leesunahhh@gmail.com>
2025-11-13 15:51:45 +0900

    fix: delete time.sleep

--- project_root/src/pages/agent_page.py
+++ project_root/src/pages/agent_page.py
@@ -97,8 +97,6 @@ class AgentPage:
             EC.element_to_be_clickable(self.save_button)
         ).click()
 
-<<<<<<< HEAD
-=======
     def upload_file(self, file_path):
         self.wait = WebDriverWait(self.driver, 10)
         project_root = os.path.dirname(
@@ -109,4 +107,3 @@ class AgentPage:
             EC.presence_of_element_located((By.XPATH, "(//input[@type='file'])[2]"))
         )
         file_input.send_keys(file_path)
->>>>>>> feature_account
