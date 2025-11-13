e219e242e331c20ceb9daad534335f07c08936fb
최고은 <sh0308ee@gmail.com>
2025-11-13 14:30:36 +0900

    fix : newchat 수정 test : scroll 파일 생성

--- project_root/tests/test_newchat.py
+++ project_root/tests/test_newchat.py
@@ -7,6 +7,7 @@ from src.pages.login_page import LoginPage
 def test_HIST001_newchat_create(driver,send_test_message,login) :
     
     # 새 채팅 세션을 만들기 위한 기존 세션 생성
+    time.sleep(2)
     send_test_message("새 채팅을 테스트하기 위한 테스트 채팅 메일입니다(답장하지마)")
 
     # 새 채팅 세션 생성
